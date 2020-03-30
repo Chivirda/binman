0e2d64e2fff4811acd4cb2ec17ae299deddb14e7
Serg Ch <chivirdos@gmail.com>
2020-03-31 00:51:55 +0300

    Добавил затемнение для модального окна

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,9 @@
 const addButton = document.querySelector('.btn-add-job-seeker')
 const divContent = document.querySelector('.content')
-const modal = document.querySelector('.modal')
-// addButton.addEventListener('click', () => {
-//     openModal()
-// })
+const modal = document.querySelector('#modal')
 
-// function openModal() {
-//     const divContent = document.querySelector('.content')
-//     const modal = document.querySelector('.modal')
-//     modal.style.width = divContent.style.width
-// }
 
-document.addEventListener('DOMContentLoaded', () => {
-
-    console.log('modalWidth:', +modal.style.width);
-    console.log('contentWidth:', divContent.style);
-    
+addButton.addEventListener('click', () => {
+    modal.classList.toggle('modal')
 })
+
