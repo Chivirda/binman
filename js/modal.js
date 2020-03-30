e3a8d679bca4203f7abbee244061d3482276c2b8
Serg Ch <chivirdos@gmail.com>
2020-03-31 00:38:01 +0300

    поменял высоту навигации

---
+++ js/modal.js
@@ -0,0 +1,19 @@
+const addButton = document.querySelector('.btn-add-job-seeker')
+const divContent = document.querySelector('.content')
+const modal = document.querySelector('.modal')
+// addButton.addEventListener('click', () => {
+//     openModal()
+// })
+
+// function openModal() {
+//     const divContent = document.querySelector('.content')
+//     const modal = document.querySelector('.modal')
+//     modal.style.width = divContent.style.width
+// }
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    console.log('modalWidth:', +modal.style.width);
+    console.log('contentWidth:', divContent.style);
+    
+})
