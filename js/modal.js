3e9e2b4f3f10487047193ece6fbe0ecba5dbf39c
Sergey Chivirda <si.chivirda@gmail.com>
2020-04-02 19:23:13 +0300

    Реализовал вывод имени загруженного файла и сброс формы. Осталось сделать сброс имени файла.

--- js/modal.js
+++ js/modal.js
@@ -2,18 +2,24 @@ const addButton = document.querySelector('.btn-add-job-seeker')
 const modal = document.querySelector('#modal')
 const closeModal = document.querySelector('.modal__close')
 
-const fileName = document.querySelector('.form__file-name')
-const inputPhoto = document.querySelector('#photo')
+const fileInputs = document.querySelectorAll('input[type="file"]')
+const btnCancel = document.querySelector('.btn-cancel')
 
 addButton.addEventListener('click', () => {
     modal.classList.toggle('closed')
-    return false
 })
 
 closeModal.addEventListener('click', () => {
     modal.classList.toggle('closed')
 })
 
-inputPhoto.addEventListener('change', () => {
-    fileName.textContent = inputPhoto.files[0].name
+for (let input of fileInputs) {
+    input.addEventListener('change', () => {
+        let inputName = document.querySelector(`#${input.id}-name`)
+        inputName.textContent = input.files[0].name
+    })
+}
+
+btnCancel.addEventListener('click', () => {
+    document.querySelector('#modal-form').reset()
 })
