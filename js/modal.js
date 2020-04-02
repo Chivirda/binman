795715a878faf0287848c5989c9c70df1b9effef
Serg Ch <chivirdos@gmail.com>
2020-04-02 14:21:58 +0300

    Переделал верстку input file

--- js/modal.js
+++ js/modal.js
@@ -2,6 +2,8 @@ const addButton = document.querySelector('.btn-add-job-seeker')
 const modal = document.querySelector('#modal')
 const closeModal = document.querySelector('.modal__close')
 
+const fileName = document.querySelector('.form__file-name')
+const inputPhoto = document.querySelector('#photo')
 
 addButton.addEventListener('click', () => {
     modal.classList.toggle('closed')
@@ -9,4 +11,9 @@ addButton.addEventListener('click', () => {
 
 closeModal.addEventListener('click', () => {
     modal.classList.toggle('closed')
+})
+
+inputPhoto.addEventListener('change', () => {
+    console.log(inputPhoto.files[0]);
+    
 })
