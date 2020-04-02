3e9a108d9688d3caf9268d6991812ffca0b02702
Sergey Chivirda <si.chivirda@gmail.com>
2020-04-02 22:36:22 +0300

    Реализовал обнуление имён выбранных файлов после сброса формы

--- js/modal.js
+++ js/modal.js
@@ -5,6 +5,11 @@ const closeModal = document.querySelector('.modal__close')
 const fileInputs = document.querySelectorAll('input[type="file"]')
 const btnCancel = document.querySelector('.btn-cancel')
 
+const fileNames = []
+    fileNames.push(document.querySelector('#photo-name'))
+    fileNames.push(document.querySelector('#resume-name'))
+    fileNames.push(document.querySelector('#archive-name'))
+
 addButton.addEventListener('click', () => {
     modal.classList.toggle('closed')
 })
@@ -22,4 +27,7 @@ for (let input of fileInputs) {
 
 btnCancel.addEventListener('click', () => {
     document.querySelector('#modal-form').reset()
+    for (let name of fileNames) {
+        name.textContent = 'Файл не выбран'
+    }
 })
